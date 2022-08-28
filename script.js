1eb51764d1867386429efb3cd549e5cac118f2c5
theimpostor <sahirhoda@gmail.com>
2022-08-28 11:32:25 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const c = document.getElementById('canvas').getContext('2d')
+
+c.fillStyle = 'black'
+c.strokeRect(0, 0, 500, 400)
+
+c.fillStyle = 'blue'
+c.fillRect(10, 10, 480, 380)
+
+c.fillStyle = 'orange'
+c.font = 'small-caps bold 24px sans-serif'
+c.textAlign = 'center'
+c.fillText('Hello code hackerz!', 250, 200)
